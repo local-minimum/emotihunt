72b212f46dd896eb3b85743dd54202be9568b3fa
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-07-01 23:13:25 +0200

    Started harris corners

---
+++ Assets/scripts/ImageAnalysis/Textures/HarrisCornerTexture.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+
+namespace ImageAnalysis.Textures
+{
+    public class HarrisCornerTexture : ConvolvableTexture
+    {
+        Filter sobelX = Filter.Get<Filters.SobelX>();
+        Filter sobelY = Filter.Get<Filters.SobelY>();
+        Filter gauss3Filter = Filter.Get<Filters.Gaussian5x5S3>();
+
+        Color[] gauss3;
+        Color[] edgeX;
+        Color[] edgeY;
+
+        Color[] response;
+
+        int gaussStride;
+        int gaussHeight;
+
+        int sobelStride;
+        int sobelHeight;
+
+        public HarrisCornerTexture(Texture2D texture) : base(texture)
+        {
+            gaussStride = texture.width - gauss3Filter.Kernel.GetLength(1) + 1;
+            gaussHeight = texture.height - gauss3Filter.Kernel.GetLength(0) + 1;
+            gauss3 = new Color[gaussHeight * gaussStride];
+
+            sobelStride = gaussStride - sobelX.Kernel.GetLength(1) + 1;
+            sobelHeight = gaussHeight - sobelX.Kernel.GetLength(0) + 1;
+
+            int edgeSizes = gaussHeight * gaussStride;
+            edgeX = new Color[edgeSizes];
+            edgeY = new Color[edgeSizes];
+
+            response = new Color[edgeSizes];
+        }
+
+        protected override void _Convolve(Color[] data, int stride)
+        {
+
+            ImageAnalysis.Convolve.Valid(data, stride, ref gauss3, gaussStride, gauss3Filter);
+
+            ImageAnalysis.Convolve.Valid(gauss3, gaussStride, ref edgeX, sobelStride, sobelX);
+            ImageAnalysis.Convolve.Valid(gauss3, gaussStride, ref edgeY, sobelStride, sobelY);
+
+            ImageAnalysis.Convolve.Resize(response, gaussStride, gaussHeight, Texture.width, ref target);
+
+        }
+    }
+}
