32b081c761d06f78ac3b34fc5ac6270c42e62305
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-07-10 13:14:40 +0200

    Working on editor

---
+++ Assets/scripts/EmotiHunt/EditorUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class EditorUI : MonoBehaviour {
+
+    string emojiName;
+    [SerializeField] Texture2D tex;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetName(Text text)
+    {
+        emojiName = text.text;
+    }
+
+    public void Serialize(Button button)
+    {
+        StartCoroutine(analyze(button));
+    }
+
+    IEnumerator<WaitForEndOfFrame> analyze(Button button) {
+
+        button.interactable = false;
+
+        yield return new WaitForEndOfFrame();
+
+
+        button.interactable = true;
+    }
+}
---
+++ Assets/scripts/EmotiHunt/Emoji.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using ImageAnalysis;
+
+[Serializable]
+public struct Emoji
+{
+    public string name;
+    public string secret;    
+    public long hash;
+
+    public Color[] pixels;
+    public int pixelStride;
+    public int height;
+
+    public Coordinate[] points;
+
+}
