c81a05a8790f3b09225fdf34ea8c5825b5742011
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-07-16 17:07:32 +0200

    Working towards some rotation

--- Assets/scripts/EmotiHunt/EmojiProjection.cs
+++ Assets/scripts/EmotiHunt/EmojiProjection.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class EmojiProjection : MonoBehaviour {
 
-    public Vector2 coordinate;
     public Image sourceImage;
     Detector detector;
     Image selfImage;
@@ -53,28 +52,112 @@ public class EmojiProjection : MonoBehaviour {
         Vector2[] emojiCorners = emoji.corners.ToVector2();
 
         //TODO: Random take 3 or something
-        Vector2 vEm1 = emojiCorners[5] - emojiCorners[3];
-        Vector2 vEm2 = emojiCorners[9] - emojiCorners[3];
+        int idE0 = 3;
+        int idE1 = 5;
+        int idE2 = 9;
+        Vector2 vEm1 = emojiCorners[idE1] - emojiCorners[idE0];
+        Vector2 vEm2 = emojiCorners[idE2] - emojiCorners[idE0];
+        float e1 = vEm1.magnitude;
+        float e2 = vEm2.magnitude;
+        float e3 = (vEm2 - vEm1).magnitude;
 
-        float val = TriangleRotAndScaleMatch(vEm1, vEm2, corners, 1, 7, 11);
-        
-        coordinate = corners[0];
-        Vector2 v = coordinate;
-        transform.localPosition = new Vector3(v.x * sourceImage.rectTransform.rect.width, v.y * sourceImage.rectTransform.rect.height);
-        selfImage.enabled = true;
+        bool first = true;
+        int bestA = 0;
+        int bestB = 0;
+        int bestC = 0;
+        float bestVal = 0;
+        int l = corners.Length;
+        for (int a=0; a< l; a++)
+        {
+            for (int b=0; b< l; b++)
+            {
+                if (b == a)
+                {
+                    continue;
+                }
+
+                for (int c=0; c<l; c++)
+                {
+                    if (c == a || c == b)
+                    {
+                        continue;
+                    }
+                    float val = TriUniformSSSTest(e1, e2, e3, corners, a, b, c);
+                    if (val < bestVal || first)
+                    {
+                        bestVal = val;
+                        bestA = a;
+                        bestB = b;
+                        bestC = c;
+                        first = false;
+                    }
+                }
+            }
+        }
+
+        if (!first)
+        {
+            Vector2 v = (corners[bestA] + corners[bestB] + corners[bestC]) / 3f;
+            transform.localPosition = new Vector3(v.x * sourceImage.rectTransform.rect.width, v.y * sourceImage.rectTransform.rect.height);
+            selfImage.enabled = true;
+        }
     }
 
-    static float TriangleRotAndScaleMatch(Vector2 emojiV1, Vector2 emojiV2, Vector2[] imgCorners, int i0, int i1, int i2)
+    static float TriUniformSSSTest(float e1, float e2, float e3, Vector2[] imgCorners, int id0, int id1, int id2)
     { 
 
-        Vector2 imgV1 = imgCorners[i1] - imgCorners[i0];
-        Vector2 imgV2 = imgCorners[i2] - imgCorners[i0];
+        Vector2 imgV1 = imgCorners[id1] - imgCorners[id0];
+        Vector2 imgV2 = imgCorners[id2] - imgCorners[id0];
 
-        float d1 = Vector2.Dot(imgV1, emojiV1);
-        float d2 = Vector2.Dot(imgV2, emojiV2);
-        return Mathf.Pow(d1 - d2, 2);
+        float i1 = imgV1.magnitude;
+        float i2 = imgV1.magnitude;
+        float i3 = (imgV2 - imgV1).magnitude;
+
+        float r1 = e1 / i1;
+        float r2 = e2 / i2;
+        float r3 = e3 / i3;
+
+        float mu = (r1 + r2 + r3) / 3f;
+
+        return (Mathf.Pow(r1 - mu, 2f) + Mathf.Pow(r2 - mu, 2f) + Mathf.Pow(r3 - mu, 2f)) / 3f;
     }
 
+
+    static float GetScale(float e1, float e2, float e3, Vector2[] imgCorners, int id0, int id1, int id2)
+    {
+        Vector2 imgV1 = imgCorners[id1] - imgCorners[id0];
+        Vector2 imgV2 = imgCorners[id2] - imgCorners[id0];
+
+        float i1 = imgV1.magnitude;
+        float i2 = imgV1.magnitude;
+        float i3 = (imgV2 - imgV1).magnitude;
+
+        float r1 = e1 / i1;
+        float r2 = e2 / i2;
+        float r3 = e3 / i3;
+
+        return (r1 + r2 + r3) / 3f;
+    }
+
+    static Vector2 GetOffset(Vector2 a, Vector2 b, Vector2 c, Vector2 A, Vector2 B, Vector2 C)
+    {
+        return ((A - a) + (B - b) + (C - c)) / 3f;
+    }
+
+    static Vector2[] GetTranslation(float e1, float e2, float e3, Vector2[] eCorners, int idE0, int idE1, int idE2, Vector2[] imgCorners, int id0, int id1, int id2)
+    {
+        float s = GetScale(e1, e2, e3, imgCorners, id0, id1, id2);
+        Vector2 o = GetOffset(eCorners[idE0], eCorners[idE1], eCorners[idE2], imgCorners[id0], imgCorners[id1], imgCorners[id2]);
+
+        int l = eCorners.Length;
+        Vector2[] newCorners = new Vector2[l];
+
+        for (int i=0; i<l; i++)
+        {
+            newCorners[i] = (eCorners[i] + o) * s;
+        }
+        return newCorners;
+    }
 
     private void SetSelfImage(Emoji emoji)
     {
