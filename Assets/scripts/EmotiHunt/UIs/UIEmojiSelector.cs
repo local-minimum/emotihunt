dae94da4072300db22144be63e8250cbc9628a68
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-07-22 18:20:33 +0200

    Working on selecting emojis to capture

---
+++ Assets/scripts/EmotiHunt/UIs/UIEmojiSelector.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIEmojiSelector : MonoBehaviour {
+
+    UISelectionMode ui;
+    Button btn;
+    Emoji emoji;
+
+    public bool Selected
+    {
+        get
+        {
+            return !btn.interactable;
+        }
+
+        set
+        {
+            btn.interactable = !value;
+        }
+    }
+
+    void Load(Emoji emoji)
+    {
+        gameObject.name = "Emoji: " + emoji.emojiName;
+        this.emoji = emoji;
+        Selected = false;
+    }
+
+	// Use this for initialization
+	void Start () {
+        btn = GetComponent<Button>();
+        ui = GetComponentInParent<UISelectionMode>();
+
+    }
+
+    public void OnClick()
+    {
+        Selected = ui.UIEmojiSelect(this);
+    }
+}
