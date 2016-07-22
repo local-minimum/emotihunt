d2d86ea73894ddf131adf1ed5e9508203ecc1448
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-07-22 18:54:35 +0200

    More functionality towards selecting emojis in image

--- Assets/scripts/EmotiHunt/UIs/UIEmojiSelector.cs
+++ Assets/scripts/EmotiHunt/UIs/UIEmojiSelector.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-
+using ImageAnalysis;
 
 public class UIEmojiSelector : MonoBehaviour {
 
     UISelectionMode ui;
     Button btn;
     Emoji emoji;
+    Texture2D tex;
+    Image img;
+    Sprite sprite;
 
     public bool Selected
     {
@@ -21,11 +24,32 @@ public class UIEmojiSelector : MonoBehaviour {
         }
     }
 
+    public string Name
+    {
+        get
+        {
+            return emoji.emojiName;
+        }
+    }
+
+    public Sprite EmojiSprite
+    {
+        get
+        {
+            return sprite;
+        }
+    }
+
+
     void Load(Emoji emoji)
     {
         gameObject.name = "Emoji: " + emoji.emojiName;
         this.emoji = emoji;
         Selected = false;
+        tex = new Texture2D(75, 75);        
+        Convolve.Apply(ref emoji.pixels, emoji.pixelStride, tex);
+        sprite = Sprite.Create(tex, new Rect(0, 0, 75, 75), Vector2.one * 0.5f);
+        img.sprite = sprite;
     }
 
 	// Use this for initialization
--- Assets/scripts/EmotiHunt/UIs/UISelectionMode.cs
+++ Assets/scripts/EmotiHunt/UIs/UISelectionMode.cs
@@ -3,18 +3,11 @@ using System.Collections;
 
 public class UISelectionMode : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     public bool UIEmojiSelect(UIEmojiSelector btn)
     {
+        
         return false;
     }
 }
