27c85a998a36300a26757edfe729579b62dd67fa
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-07-22 19:16:46 +0200

    Selection UI start of selection

---
+++ Assets/scripts/EmotiHunt/UIs/UIEmojiSelected.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIEmojiSelected : MonoBehaviour {
+
+    [SerializeField]
+    Sprite emptySprite;
+
+    UIEmojiSelector btn;
+    Button button;
+    Image img;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        img = GetComponent<Image>();
+    }
+
+    public bool Free
+    {
+        get
+        {
+            return btn == null;
+        }
+    }
+
+    public void Set(UIEmojiSelector btn)
+    {
+        this.btn = btn;
+        img.sprite = btn.EmojiSprite;
+        button.interactable = true;
+    }
+
+    public void Unset()
+    {
+        btn = null;
+        img.sprite = emptySprite;
+        button.interactable = false;
+    }
+
+    public void Shift(UIEmojiSelected other)
+    {
+        other.Set(btn);
+        Unset();
+    }
+}
--- Assets/scripts/EmotiHunt/UIs/UISelectionMode.cs
+++ Assets/scripts/EmotiHunt/UIs/UISelectionMode.cs
@@ -3,11 +3,33 @@ using System.Collections;
 
 public class UISelectionMode : MonoBehaviour {
 
-	
+    UIEmojiSelected[] selections;
+
+    void Start()
+    {
+        selections = GetComponentsInChildren<UIEmojiSelected>();
+    }
 
     public bool UIEmojiSelect(UIEmojiSelector btn)
     {
-        
+        for (int i=0; i<selections.Length; i++)
+        {
+            if (selections[i].Free)
+            {
+                selections[i].Set(btn);
+                return true;
+            }
+        }
+
         return false;
+    }
+
+    public void RemoveSelection(UIEmojiSelected selected)
+    {
+        for (int i=System.Array.IndexOf(selections, selected), l = selections.Length - 1; i<l; i++)
+        {
+            selections[i + 1].Shift(selections[i]);
+        }
+        selections[selections.Length - 1].Unset();
     }
 }
