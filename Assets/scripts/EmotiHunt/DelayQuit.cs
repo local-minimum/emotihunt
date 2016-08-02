d4c5b4a6bc8a37f861d3f67e870fdea3a060a2cd
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-08-02 11:44:30 +0200

    Quit sequence

---
+++ Assets/scripts/EmotiHunt/DelayQuit.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DelayQuit : MonoBehaviour {
+
+    MobileUI mobileUI;
+    bool quitting = false;
+    [SerializeField, Range(0, 3)] float delay = 1;
+
+	void Awake () {
+        mobileUI = GetComponentInParent<MobileUI>();
+	}
+	
+    void OnEnable()
+    {
+        mobileUI.OnModeChange += HandleMode;
+    }
+
+    void OnDisable()
+    {
+        mobileUI.OnModeChange -= HandleMode;
+    }
+
+    private void HandleMode(UIMode mode)
+    {
+        if (mode == UIMode.Quitting)
+        {
+            StartCoroutine(Delay());
+        } else
+        {
+            quitting = false;
+        }
+    }
+
+    IEnumerator<WaitForSeconds> Delay() {
+        if (quitting)
+        {
+            yield break;           
+        }
+        mobileUI.SetStatus("Bye bye");
+        quitting = true;
+        yield return new WaitForSeconds(delay);
+        if (quitting)
+        {
+            mobileUI.Abort();
+        }
+	}
+}
