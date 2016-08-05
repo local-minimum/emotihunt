138d3a82745a62b97a717f3b1bb212b2cf9d9b36
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-08-05 16:03:20 +0200

    Feed is working

---
+++ Assets/scripts/EmotiHunt/UIs/ExpandToContent.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExpandToContent : MonoBehaviour {
+
+    float size = -1;
+
+    VerticalLayoutGroup layoutGroup;
+
+    void Awake()
+    {
+        layoutGroup = GetComponent<VerticalLayoutGroup>();
+
+    }
+
+	void Update () {
+        float newSize = ContentHeight;
+
+        if (size != newSize)
+        {
+            RectTransform rt = transform as RectTransform;
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x, newSize);
+            size = newSize;
+        }    
+	}
+
+    float ContentHeight
+    {
+        get
+        {
+            RectTransform rt = transform as RectTransform;
+
+            float height = 0;
+            for (int i = 0; i < rt.childCount; i++)
+            {
+                RectTransform child = rt.GetChild(i) as RectTransform;
+                height += child.sizeDelta.y;
+                if (i > 0)
+                {
+                    height += layoutGroup.spacing;
+                }
+
+            }
+            return height;
+            
+        }
+    }
+
+
+}
